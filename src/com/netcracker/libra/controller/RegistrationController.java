2d6f0af4a158245ea29767de05d9dfff056cc755
Konstantin Kuyun <disapp0int@gmail.com>
2013-01-26 16:42:11 +0200

    Перемещена главная страница

--- src/com/netcracker/libra/controller/RegistrationController.java
+++ src/com/netcracker/libra/controller/RegistrationController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.netcracker.libra.controller;
 
 import com.netcracker.libra.dao.StudentJDBC;
