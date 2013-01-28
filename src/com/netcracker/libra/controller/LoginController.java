2f0ed2b5403df0e6c7d180774396fd563fc831fc
Konstantin Kuyun <disapp0int@gmail.com>
2013-01-28 23:16:37 +0200

    Исправления

--- src/com/netcracker/libra/controller/LoginController.java
+++ src/com/netcracker/libra/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.netcracker.libra.dao.StudentJDBC;
@@ -25,7 +24,7 @@ public class LoginController {
 		 this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
 	
-   @RequestMapping(value = "/login", method = RequestMethod.GET)
+   @RequestMapping(value = "/login.html", method = RequestMethod.GET)
    public ModelAndView login() {
       return new ModelAndView("login", "command", new Student());
    }
