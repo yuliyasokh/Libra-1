dcddfeaf810a4ffe1dae5d8494b2c74f6a1ed99e
Yuliya <Yuliya@yuliya>
2013-02-03 00:33:32 +0200

    Great commit

--- src/com/netcracker/libra/dao/HrJDBC.java
+++ src/com/netcracker/libra/dao/HrJDBC.java
@@ -1,12 +1,17 @@
 package com.netcracker.libra.dao;
 
+import com.netcracker.libra.model.SignupForm;
+import com.netcracker.libra.model.Student;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class HrJDBC implements HrDAO {
+
+       private static JdbcTemplate jdbcTemplateObject;
 
 	@Override
 	public void create(String name, String lastName, String email,
@@ -27,11 +32,21 @@ public class HrJDBC implements HrDAO {
 		// TODO Auto-generated method stub
 		
 	}
-
+        @Override
+        public void deleteFormById(Integer id){
+           String SQL = "delete from AppForm where appid = ?)";
+           jdbcTemplateObject.update(SQL, id);
+           System.out.println("Deleted form with ID = " + id );
+        }
+        @Override
+        public void updateStudent(SignupForm form){
+          
+        }
 	@Override
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
-		// TODO Auto-generated method stub
+            jdbcTemplateObject = new JdbcTemplate(dataSource);
+
 		
 	}
 
